1a87c2b5f4adabc6971c325243cd771b09272f90
Adarsha-React <adarshareact2023@gmail.com>
2023-06-07 16:06:17 +0530

    Tested Javascript_JSX_ImageLoad_Adding Routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,39 @@
+import appLogo from "../assets/images/appLogo.png";
+
 const Header = () => {
   return (
-    <div>
-      <h1>Header component</h1>
+    <div className="container mx-auto mt-2">
+      <div className="flex shadow-md">
+        <div className="w-1/2">
+          <img className="w-12 h-12" src={appLogo} alt="App Logo" />
+        </div>
+        <div className=" w-3/2 container mx-auto flex justify-between mr-10 mt-2">
+          <div className="container mx-auto flex justify-between">
+            <div className="mx-2 w-3/2">
+              <input
+                className="mx-2 p-1 outline-none border border-1 text-sm w-80"
+                type="text"
+                placeholder="Search"
+              />
+              <button className="bg-gray-200 font-semibold text-xs px-2 py-1 rounded-md">
+                Search
+              </button>
+            </div>
+            <div className="flex">
+              <div className="mx-2">
+                <h1 className="bg-gray-200 font-semibold text-xs px-2 py-1 rounded-md">
+                  About
+                </h1>
+              </div>
+              <div className="mx-2">
+                <h1 className="bg-gray-200 font-semibold text-xs px-2 py-1 rounded-md">
+                  Cart
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppLayout from "./App";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
+function AppLayout() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
